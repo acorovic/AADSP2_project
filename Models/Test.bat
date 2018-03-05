6792e0dd003cf1d03e6284c0375c299a590b8403
acorovic <aleksa_corovic@uns.ac.rs>
2018-03-05 14:39:06 +0100

    Add model1, script for testing

--- Models/Test.bat
+++ Models/Test.bat
@@ -7,25 +7,28 @@ del whiteNoise_Model2_vs_Model3.txt
 cd ..
 
 : Execute Model 0, Model 1, Model 2 and Model 3
-cd Debug
-"multitapEcho_model0.exe" "..//..//TestStreams//speech.wav" "..//OutStreams//out_speech_0.wav" 
-"multitapEcho_model1.exe" "..//..//TestStreams//speech.wav" "..//OutStreams//out_speech_1.wav"
-"multitapEcho_model2.exe" "..//..//TestStreams//speech.wav" "..//OutStreams//out_speech_2.wav" 
+cd ProcessWavFile/Debug
+"ProcessWavFile.exe" "..//..//..//TestStreams//2ch_contour_ne40_24b_48k.wav" "..//..//OutStreams//cmp_0.wav" 2
+cd ../..
+
+cd model1/Debug
+"model1.exe" "..//..//..//TestStreams//2ch_contour_ne40_24b_48k.wav" "..//..//OutStreams//cmp_1.wav" 2
+
 
 :: TO DO: Call model 1 executable and name output file: out_speech_1.wav
 :: TO DO: Call model 2 executable and name output file: out_speech_2.wav
 
-cd ..
+: cd ..
 
-cd multitapEcho_model3
-c:\CirrusDSP\bin\cmdline_simulator.exe -project SimulatorConfigurationTemp.sbr -max_cycles 1000000
+: cd multitapEcho_model3
+: c:\CirrusDSP\bin\cmdline_simulator.exe -project SimulatorConfigurationTemp.sbr -max_cycles 1000000
 
-cd ..
+cd ../..
 
 : Generate new logs
-"..//tools//PCMCompare.exe" OutStreams//out_speech_0.wav OutStreams//out_speech_1.wav 2> OutCmp//whiteNoise_Model0_vs_Model1.txt
-"..//tools//PCMCompare.exe" OutStreams//out_speech_1.wav OutStreams//out_speech_2.wav 2> OutCmp//whiteNoise_Model1_vs_Model2.txt
-"..//tools//PCMCompare.exe" OutStreams//out_speech_2.wav OutStreams//out_speech_3.wav 2> OutCmp//whiteNoise_Model2_vs_Model3.txt
+"..//tools//PCMCompare.exe" OutStreams//cmp_0.wav OutStreams//cmp_1.wav 2> OutCmp//speech_Model0_vs_Model1.txt
+: "..//tools//PCMCompare.exe" OutStreams//out_speech_1.wav OutStreams//out_speech_2.wav 2> OutCmp//whiteNoise_Model1_vs_Model2.txt
+: "..//tools//PCMCompare.exe" OutStreams//out_speech_2.wav OutStreams//out_speech_3.wav 2> OutCmp//whiteNoise_Model2_vs_Model3.txt
 
 :: TO DO: Compare output of model1 and model2 and store the result in OutCmp//whiteNoise_Model1_vs_Model2.txt
 
---
+++ Models/model1/model1/common.h
@@ -0,0 +1,16 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+/* Basic constants */
+/* TO DO: Move defined constants here */
+
+#define BLOCK_SIZE 16
+#define MAX_NUM_CHANNEL 8
+
+/* DSP type definitions */
+typedef short DSPshort;					/* DSP integer */
+typedef unsigned short DSPushort;		/* DSP unsigned integer */
+typedef int DSPint;						/* native integer */
+typedef double DSPfract;				/* DSP fixed-point fractional */
+
+#endif
